6fb061415a4a6c9e0cd94ef8e04bce6600bf0726
Neil MacDonald <FortOyer@hotmail.co.uk>
2017-10-24 01:22:38 +0100

    Added additional test case. Improved build instructions.

--- README.md
+++ README.md
@@ -4,10 +4,18 @@ A library for N-dimension geometry maths.
 
 ## Building
 
+### Linux
+
 1. mkdir build
 2. cd build
 3. cmake ..
 4. make
+
+### Windows Visual Studio 2015 x64
+1. mkdir build
+2. cd build
+3. cmake -G "Visual Studio 14 2015 Win64" ..
+4. nmake
 
 ## Testing
 
--- test/test_fold.cpp
+++ test/test_fold.cpp
@@ -4,14 +4,29 @@
 #include <dimmath/sum.h>
 #include <dimmath/distance.h>
 
-TEST_CASE( "Sums are computed", "[vector]" ) {
+#include <string>
+
+TEST_CASE( "Sums are computed", "[fold]" )
+{
   REQUIRE(dimmath::sum(std::array<double, 2> { 1.2, 3.3, }) == 4.5);
   REQUIRE(dimmath::sum(std::array<int, 1> { 1 }) == 1);
   REQUIRE(dimmath::sum(std::array<float, 3> { 1.0f/3.0f, 0.0f }) == 1.0f/3.0f);
 }
 
-TEST_CASE( "Distances are computed", "[vector]" ) {
+TEST_CASE( "Distances are computed", "[fold]" )
+{
   REQUIRE(dimmath::distance_squared(std::array<double, 2> { 4.0, 2.0, }) == 4.0*4.0 + 2.0*2.0);
   REQUIRE(dimmath::distance(std::array<int, 2> { 4, 2, }) == std::sqrt(4.0*4.0 + 2.0*2.0));
   REQUIRE(dimmath::distance(std::array<float, 3> { -5.0f, 1.0f, 100.0f }) == std::sqrt(-5.0f*-5.0f + 1.0f + 100.0f*100.0f));
+}
+
+TEST_CASE("Different output form", "[fold]")
+{
+  REQUIRE(dimmath::foldr(
+        [](double x, std::string y) -> std::string
+        {
+          return y + std::to_string(static_cast<int>(x));
+        },
+        std::string { "Hello" },
+        std::array<double, 5> { 1.2, 2.22, 3.13431, 4.999999 }) == "Hello1234");
 }
